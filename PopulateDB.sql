94e6ca6b6a3fb1bc0238c7bde62d246485d739d8
Chitanu Victor <IujyrinoT2@protonmail.com>
2023-10-11 16:55:50 +0300

    Created scripts to populate or empty the database and updated readme

---
+++ PopulateDB.sql
@@ -0,0 +1,19 @@
+insert into meddb.public.address (id, country, city, street, suite, floor, appartment) values (1, 'Romania', 'Bucharest', 'str1', 'suite1', 1, 1);
+insert into meddb.public.person (id, first_name, last_name, email, address_id) values (1, 'Ioan', 'Gigel', 'email1@email.com', 1);
+insert into meddb.public.medic (id, title, "personId") values (1, 'Optometrist', 1);
+
+insert into meddb.public.address (id, country, city, street, suite, floor, appartment) values (2, 'Romania', 'Bacau', 'str2', 'suite2', 2, 2);
+insert into meddb.public.person (id, first_name, last_name, email, address_id) values (2, 'John', 'Smith', 'email2@email.com', 2);
+insert into meddb.public.medic (id, title, "personId") values (2, 'Physician', 2);
+
+insert into meddb.public.address (id, country, city, street, suite, floor, appartment) values (3, 'Romania', 'Timisoara', 'str3', 'suite3', 3, 3);
+insert into meddb.public.person (id, first_name, last_name, email, address_id) values (3, 'Aminah', 'Shields', 'email3@email.com', 3);
+insert into meddb.public.medic (id, title, "personId") values (3, 'Podiatrist', 3);
+
+insert into meddb.public.address (id, country, city, street, suite, floor, appartment) values (4, 'Romania', 'Suceava', 'str4', 'suite4', 4, 4);
+insert into meddb.public.person (id, first_name, last_name, email, address_id) values (4, 'Brodie', 'Garza', 'email4@email.com', 4);
+insert into meddb.public.medic (id, title, "personId") values (4, 'Chiropractor', 4);
+
+insert into meddb.public.address (id, country, city, street, suite, floor, appartment) values (5, 'Romania', 'Bacau', 'str5', 'suite5', 5, 5);
+insert into meddb.public.person (id, first_name, last_name, email, address_id) values (5, 'Jac', 'Booker', 'email5@email.com', 5);
+
--- README.md
+++ README.md
@@ -25,9 +25,12 @@ services:
 ```
 4. create a ".env" file with all the postgres credential to help prisma connect to the DB
 like so: ```DATABASE_URL="postgresql://root:123@localhost:5432/meddb?schema=public"```  
-4. start the database server with ```# docker-compose up```  
-5. generate database tables with ```$ npx prisma generate```  
-5. run the nest server with either ```$ npm start``` or ```$ npm run dev```  
-6. server listens to port 3000 and can accept GET requests on "/medic" or "/medic/<id>"  
-7. Prisma Studio can be started with ```$ npx prisma studio``` and opens on localhost:5555  
-8. GraphQL Playground opens on localhost:3000/graphql  
+5. start the database server with ```# docker-compose up```  
+6. generate prisma client with ```$ npx prisma generate```  
+7. generate database tables with '''$ npx prisma migrate dev --name init'''   
+8. populate database with ```PGPASSWORD=123 psql -h localhost -U root -p 5432 -d meddb -a -q -f PopulateDB.sql```
+or empty the db with ```PGPASSWORD=123 psql -h localhost -U root -p 5432 -d meddb -a -q -f EmptyDB.sql```
+9. run the nest server with either ```$ npm start``` or ```$ npm run dev```  
+10. server listens to port 3000 and can accept GET requests on "/medic" or "/medic/<id>"  
+11. Prisma Studio can be started with ```$ npx prisma studio``` and opens on localhost:5555  
+12. GraphQL Playground opens on localhost:3000/graphql  
